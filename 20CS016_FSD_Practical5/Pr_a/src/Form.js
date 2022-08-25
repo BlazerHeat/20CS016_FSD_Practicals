7ecc9265cc052a4fcc77b9538cedcb2896b282a4
BlazerHeat <vghoghari82@gmail.com>
2022-08-25 10:57:00 +0530

    Add practical 5

---
+++ 20CS016_FSD_Practical5/Pr_a/src/Form.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Form.css';
+import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
+
+const Form = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+    alert("Form submitted!");
+  }
+  return (
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>×</span>
+        <div className='form-content-left'>
+          <img className='form-img' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVtv5uMX-G99fIXngMKTu_OF268qGeHpd5Kg&usqp=CAU' alt='spaceship' />
+        </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
